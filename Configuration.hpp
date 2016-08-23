6e9bbde87cf67a295c3c7369d8b0c3efa4945aeb
Adrian <adrian@vailacom.net>
2016-08-23 03:51:32 +0200

    - Make scan asynchronous - Sort networks by signal strength

--- Configuration.hpp
+++ Configuration.hpp
@@ -1,13 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
+#include <vector>
+#include <algorithm>
 
 #include "Resources.h"
 
 class Configuration {
 public:
     Configuration() { }
-    bool begin(const char *ssid, const char *pass = NULL) {
+    bool begin(const char *ssid, const char *pass = nullptr) {
         WiFi.mode(WIFI_AP_STA);
 
         if (pass) WiFi.softAP(ssid, pass);
@@ -23,32 +25,27 @@ public:
             page.replace("%ssid%", WiFi.SSID());
             page.replace("%pass%", WiFi.psk());
             
-            // Todo: make this an ajax call with preloader
-            int n = WiFi.scanNetworks();
-            if (n == 0) {
-                page.replace("%scan_results%", "<tr><td>No networks found. Refresh to scan again.</td></tr>");
-            } else {
-                String results;
-                for (int i = 0; i < n; i++) {
-                    auto ssid = WiFi.SSID(i);
-                    auto rssi = WiFi.RSSI(i);
-
-                    String item = FPSTR(Resource_NetworkRow);
-                    item.replace("%ssid%", ssid);
-                    item.replace("%strength%", String(signalStrength(rssi)));
-                    item.replace("%fa-wifi%", FPSTR(Resource_WiFiIcon));
-                    
-                    if (WiFi.encryptionType(i) != ENC_TYPE_NONE) {
-                        item.replace("%fa-lock%", FPSTR(Resource_LockIcon));
-                    } else {
-                        item.replace("%fa-lock%", "");
-                    }
-                    results += item;
-                }
-                page.replace("%scan_results%", results);
-            }
-
             m_http.send(200, "text/html", page);
+        });
+        m_http.on("/scan", [this] {
+            String page("[");
+            int n = WiFi.scanNetworks();
+            if (n > 0) {
+                std::vector<int> v(n);
+                std::iota(v.begin(), v.end(), 0);
+                std::sort(v.begin(), v.end(), [](int a, int b) -> bool { return WiFi.RSSI(a) > WiFi.RSSI(b); });
+                
+                for (int x : v) {
+                    if (page.length() > 1) page += ',';
+                    page += "{\"ssid\": \"" + WiFi.SSID(x) + "\", " +
+                            " \"strength\": " + String(signalStrength(WiFi.RSSI(x))) + ", " +
+                            " \"open\": " + (WiFi.encryptionType(x) == ENC_TYPE_NONE? "true" : "false") + 
+                            "}";
+                }
+            }
+            page += "]";
+
+            m_http.send(200, "application/json", page);
         });
         m_http.on("/save", [this] {
             String ssid = m_http.arg("ssid").c_str();
@@ -86,7 +83,7 @@ private:
 
     bool captivePortal() {
         if (!isIp(m_http.hostHeader()) ) {
-            m_http.sendHeader("Location", String("http://") + ipToString(m_http.client().localIP()), true);
+            m_http.sendHeader("Location", String("http://") + m_http.client().localIP().toString(), true);
             m_http.send(302, "text/plain", "");
             m_http.client().stop();
             return true;
@@ -95,16 +92,7 @@ private:
     }
 
     int signalStrength(int RSSI) {
-        int quality = 0;
-
-        if (RSSI <= -100) {
-            quality = 0;
-        } else if (RSSI >= -50) {
-            quality = 100;
-        } else {
-            quality = 2 * (RSSI + 100);
-        }
-        return quality;
+        return std::max(0, std::min(2 * (RSSI + 100), 100));
     }
     bool isIp(String str) {
         for (int i = 0; i < str.length(); i++) {
@@ -114,13 +102,5 @@ private:
             }
         }
         return true;
-    }
-    String ipToString(IPAddress ip) {
-        String res = "";
-        for (int i = 0; i < 3; i++) {
-            res += String((ip >> (8 * i)) & 0xFF) + ".";
-        }
-        res += String(((ip >> 8 * 3)) & 0xFF);
-        return res;
     }
 };
--- Resources.h
+++ Resources.h
@@ -133,6 +133,14 @@ const char Resource_Index[] PROGMEM = R"RES(<!DOCTYPE html>
                 border-bottom: 1px solid #DDDDDD;
                 vertical-align: bottom;
             }
+            .loader {
+                -webkit-animation: spin 1s linear infinite;
+                -moz-animation: spin 1s linear infinite;
+                animation: spin 1s linear infinite;
+            }
+            @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
+            @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
+            @keyframes spin { 100% { -webkit-transform: rotate(360deg); transform: rotate(360deg); } }
         </style>
         <script type="text/javascript">
             function post(url, data, callback) {
@@ -154,6 +162,24 @@ const char Resource_Index[] PROGMEM = R"RES(<!DOCTYPE html>
                     window.close();
                 });
             }
+            
+            post('/scan', '', function (data) {
+                var html = '';
+                var results = JSON.parse(data);
+                if (!data || !results) {
+                    html = '<tr><td colspan="2" valign="middle" align="center">No networks found. Refresh to scan again.</td></tr>';
+                } else {
+                    for (var i in results) {
+                        var lock = !results[i].open? '<div style="position: absolute; left: 3px;"><svg width="14" height="14" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M640 768h512v-192q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-192q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z" fill="#000"/></svg></div>' : '';
+                        
+                        html += '<tr onclick="document.getElementById(\'ssid\').value = this.getElementsByTagName(\'td\')[0].getElementsByTagName(\'span\')[0].innerHTML" style="cursor: pointer;"> \
+                                    <td style="position: relative">' + lock + '<span>' + results[i].ssid + '</span></td> \
+                                    <td class="text-navy"><svg width="14" height="14" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M896 1523q-20 0-93-73.5t-73-93.5q0-32 62.5-54t103.5-22 103.5 22 62.5 54q0 20-73 93.5t-93 73.5zm270-271q-2 0-40-25t-101.5-50-128.5-25-128.5 25-101 50-40.5 25q-18 0-93.5-75t-75.5-93q0-13 10-23 78-77 196-121t233-44 233 44 196 121q10 10 10 23 0 18-75.5 93t-93.5 75zm273-272q-11 0-23-8-136-105-252-154.5t-268-49.5q-85 0-170.5 22t-149 53-113.5 62-79 53-31 22q-17 0-92-75t-75-93q0-12 10-22 132-132 320-205t380-73 380 73 320 205q10 10 10 22 0 18-75 93t-92 75zm271-271q-11 0-22-9-179-157-371.5-236.5t-420.5-79.5-420.5 79.5-371.5 236.5q-11 9-22 9-17 0-92.5-75t-75.5-93q0-13 10-23 187-186 445-288t527-102 527 102 445 288q10 10 10 23 0 18-75.5 93t-92.5 75z" fill="#1ab394" /></svg> ' + results[i].strength +'%</td> \
+                                </tr>';
+                    }
+                }
+                document.getElementById('scan_results').innerHTML = html;
+            });
         </script>
     </head>
     <body>
@@ -170,8 +196,10 @@ const char Resource_Index[] PROGMEM = R"RES(<!DOCTYPE html>
                             <th>Signal strength</th>
                         </tr>
                         </thead>
-                        <tbody>
-                        %scan_results%
+                        <tbody id="scan_results">
+                            <tr><td colspan="2" valign="middle" align="center">
+                                <svg class="loader" width="32" height="32" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="uil-ring"><path d="M10,50c0,0,0,0.5,0.1,1.4c0,0.5,0.1,1,0.2,1.7c0,0.3,0.1,0.7,0.1,1.1c0.1,0.4,0.1,0.8,0.2,1.2c0.2,0.8,0.3,1.8,0.5,2.8 c0.3,1,0.6,2.1,0.9,3.2c0.3,1.1,0.9,2.3,1.4,3.5c0.5,1.2,1.2,2.4,1.8,3.7c0.3,0.6,0.8,1.2,1.2,1.9c0.4,0.6,0.8,1.3,1.3,1.9 c1,1.2,1.9,2.6,3.1,3.7c2.2,2.5,5,4.7,7.9,6.7c3,2,6.5,3.4,10.1,4.6c3.6,1.1,7.5,1.5,11.2,1.6c4-0.1,7.7-0.6,11.3-1.6 c3.6-1.2,7-2.6,10-4.6c3-2,5.8-4.2,7.9-6.7c1.2-1.2,2.1-2.5,3.1-3.7c0.5-0.6,0.9-1.3,1.3-1.9c0.4-0.6,0.8-1.3,1.2-1.9 c0.6-1.3,1.3-2.5,1.8-3.7c0.5-1.2,1-2.4,1.4-3.5c0.3-1.1,0.6-2.2,0.9-3.2c0.2-1,0.4-1.9,0.5-2.8c0.1-0.4,0.1-0.8,0.2-1.2 c0-0.4,0.1-0.7,0.1-1.1c0.1-0.7,0.1-1.2,0.2-1.7C90,50.5,90,50,90,50s0,0.5,0,1.4c0,0.5,0,1,0,1.7c0,0.3,0,0.7,0,1.1 c0,0.4-0.1,0.8-0.1,1.2c-0.1,0.9-0.2,1.8-0.4,2.8c-0.2,1-0.5,2.1-0.7,3.3c-0.3,1.2-0.8,2.4-1.2,3.7c-0.2,0.7-0.5,1.3-0.8,1.9 c-0.3,0.7-0.6,1.3-0.9,2c-0.3,0.7-0.7,1.3-1.1,2c-0.4,0.7-0.7,1.4-1.2,2c-1,1.3-1.9,2.7-3.1,4c-2.2,2.7-5,5-8.1,7.1 c-0.8,0.5-1.6,1-2.4,1.5c-0.8,0.5-1.7,0.9-2.6,1.3L66,87.7l-1.4,0.5c-0.9,0.3-1.8,0.7-2.8,1c-3.8,1.1-7.9,1.7-11.8,1.8L47,90.8 c-1,0-2-0.2-3-0.3l-1.5-0.2l-0.7-0.1L41.1,90c-1-0.3-1.9-0.5-2.9-0.7c-0.9-0.3-1.9-0.7-2.8-1L34,87.7l-1.3-0.6 c-0.9-0.4-1.8-0.8-2.6-1.3c-0.8-0.5-1.6-1-2.4-1.5c-3.1-2.1-5.9-4.5-8.1-7.1c-1.2-1.2-2.1-2.7-3.1-4c-0.5-0.6-0.8-1.4-1.2-2 c-0.4-0.7-0.8-1.3-1.1-2c-0.3-0.7-0.6-1.3-0.9-2c-0.3-0.7-0.6-1.3-0.8-1.9c-0.4-1.3-0.9-2.5-1.2-3.7c-0.3-1.2-0.5-2.3-0.7-3.3 c-0.2-1-0.3-2-0.4-2.8c-0.1-0.4-0.1-0.8-0.1-1.2c0-0.4,0-0.7,0-1.1c0-0.7,0-1.2,0-1.7C10,50.5,10,50,10,50z" fill="#1ab394"></svg>
+                            </td></tr>
                         </tbody>
                     </table>
                 </div>
@@ -187,9 +215,3 @@ const char Resource_Index[] PROGMEM = R"RES(<!DOCTYPE html>
         </div>
     </body>
 </html>)RES";
-const char Resource_WiFiIcon[] PROGMEM = R"RES(<svg width="14" height="14" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M896 1523q-20 0-93-73.5t-73-93.5q0-32 62.5-54t103.5-22 103.5 22 62.5 54q0 20-73 93.5t-93 73.5zm270-271q-2 0-40-25t-101.5-50-128.5-25-128.5 25-101 50-40.5 25q-18 0-93.5-75t-75.5-93q0-13 10-23 78-77 196-121t233-44 233 44 196 121q10 10 10 23 0 18-75.5 93t-93.5 75zm273-272q-11 0-23-8-136-105-252-154.5t-268-49.5q-85 0-170.5 22t-149 53-113.5 62-79 53-31 22q-17 0-92-75t-75-93q0-12 10-22 132-132 320-205t380-73 380 73 320 205q10 10 10 22 0 18-75 93t-92 75zm271-271q-11 0-22-9-179-157-371.5-236.5t-420.5-79.5-420.5 79.5-371.5 236.5q-11 9-22 9-17 0-92.5-75t-75.5-93q0-13 10-23 187-186 445-288t527-102 527 102 445 288q10 10 10 23 0 18-75.5 93t-92.5 75z" fill="#1ab394" /></svg>)RES";
-const char Resource_LockIcon[] PROGMEM = R"RES(<svg width="14" height="14" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M640 768h512v-192q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-192q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z" fill="#000"/></svg>)RES";
-const char Resource_NetworkRow[] PROGMEM = R"RES(<tr onclick="document.getElementById('ssid').value = this.getElementsByTagName('td')[0].getElementsByTagName('span')[0].innerHTML" style="cursor: pointer;">
-                                                    <td style="position: relative"><div style="position: absolute; left: 3px;">%fa-lock%</div><span>%ssid%</span></td>
-                                                    <td class="text-navy">%fa-wifi% %strength%%</td>
-                                                </tr>)RES";
